da98cc1599defec3f4b3e54f7d39be4529ec29df
Aditya Bose <adi26mar@gmail.com>
2025-09-14 19:21:11 +0530

    fix: update main entrypoint to include database and routes

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from database import engine, Base
+from routers import auth, calories
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Meal Calorie Count Generator Backend")
+
+app.include_router(auth.router)
+app.include_router(calories.router)
 
 @app.get("/")
 def read_root():
